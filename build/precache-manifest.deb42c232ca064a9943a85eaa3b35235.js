9339eef5d9c6e45d7904dc2c3736c00968cde757
ThomasLee94 <thomaslee9474@gmail.com>
2020-05-15 00:21:42 -0700

    [add] build

---
+++ build/precache-manifest.deb42c232ca064a9943a85eaa3b35235.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4780f215d7c489e5025f21be2f8ae49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6e4c24d7ac76f270014",
+    "url": "/static/css/main.948f7229.chunk.css"
+  },
+  {
+    "revision": "b59e3f5b9cf3c5a9a596",
+    "url": "/static/js/2.8eb09185.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/static/js/2.8eb09185.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6e4c24d7ac76f270014",
+    "url": "/static/js/main.a4eba1ff.chunk.js"
+  },
+  {
+    "revision": "32e32d6bfc5f997bb11b",
+    "url": "/static/js/runtime-main.778c7abe.js"
+  }
+]);
